ac4060c0cccebace8a610622dd131889f259eafb
DylanQuitsGum <greg.satterlee@gmail.com>
2025-12-08 11:58:37 -0600

    added using std

--- Chapter2/Chapter2_GameStore.cpp
+++ Chapter2/Chapter2_GameStore.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    const std::string STORE_NAME = "Games and Stuff";
-    const std::string ADDRESS_STREET = "123 I Can Dr.";
-    const std::string ADDRESS_CITY = "Oklahoma City";
-    const std::string ADDRESS_STATE = "OK";
-    const std::string ADDRESS_ZIPCODE = "73170";
+    const string STORE_NAME = "Games and Stuff";
+    const string ADDRESS_STREET = "123 I Can Dr.";
+    const string ADDRESS_CITY = "Oklahoma City";
+    const string ADDRESS_STATE = "OK";
+    const string ADDRESS_ZIPCODE = "73170";
 
     const double SALES_TAX = 0.0875;
 
-    std::string GameOneTitle = "Battlefield 6";
-    std::string GameTwoTitle = "Call of Duty 7";
-    std::string GameThreeTitle = "Kerbal Space Program";
+    string GameOneTitle = "Battlefield 6";
+    string GameTwoTitle = "Call of Duty 7";
+    string GameThreeTitle = "Kerbal Space Program";
 
-    std::string firstName = "";
-    std::string lastName = "";
-    std::string streetName = "";
-    std::string city = "";
-    std::string state = "";
-    std::string zipCode = "";
+    string firstName = "";
+    string lastName = "";
+    string streetName = "";
+    string city = "";
+    string state = "";
+    string zipCode = "";
 
     double Game1Price = 69.99;
     double Game2Price = 69.99;
@@ -36,31 +38,31 @@ int main()
     double taxTotal = subTotal * SALES_TAX;
     double grandTotal = taxTotal + subTotal;
 
-    std::cout << std::endl << "Reciept" << std::endl << std::endl;
+    cout << endl << "Reciept" << endl << endl;
 
-    std::cout << STORE_NAME << std::endl;
-    std::cout << ADDRESS_STREET << std::endl;
-    std::cout << ADDRESS_CITY << ", " << ADDRESS_ZIPCODE << std::endl << std::endl;
+    cout << STORE_NAME << endl;
+    cout << ADDRESS_STREET << endl;
+    cout << ADDRESS_CITY << ", " << ADDRESS_ZIPCODE << endl << endl;
 
-    std::cout << "Bill To" << std::endl;
-    std::cout << firstName << " " << lastName << std::endl;
-    std::cout << streetName << std::endl;
-    std::cout << city << ", " << state << " " << zipCode << std::endl << std::endl;
+    cout << "Bill To" << endl;
+    cout << firstName << " " << lastName << endl;
+    cout << streetName << endl;
+    cout << city << ", " << state << " " << zipCode << endl << endl;
 
-    std::cout << "--------\t" << "-----------\t" << "\t----------\t\t" << "-----" << std::endl;
-    std::cout << "Quantity\t" << "Description\t" << "\tUnit Price\t\t" << "Total" << std::endl;
-    std::cout << "--------\t" << "-----------\t" << "\t----------\t\t" << "-----" << std::endl << std::endl;
+    cout << "--------\t" << "-----------\t" << "\t----------\t\t" << "-----" << endl;
+    cout << "Quantity\t" << "Description\t" << "\tUnit Price\t\t" << "Total" << endl;
+    cout << "--------\t" << "-----------\t" << "\t----------\t\t" << "-----" << endl << endl;
 
-    std::cout << Game1Quantity << "\t\t" << GameOneTitle << "\t\t" << Game1Price << "\t\t\t" << Game1Amount << std::endl; 
-    std::cout << Game2Quantity << "\t\t" << GameTwoTitle << "\t\t" << Game2Price << "\t\t\t" << Game2Amount << std::endl;
-    std::cout << Game3Quantity << "\t\t" << GameThreeTitle << "\t" << Game3Price << "\t\t\t" << Game3Amount << std::endl;
+    cout << Game1Quantity << "\t\t" << GameOneTitle << "\t\t" << Game1Price << "\t\t\t" << Game1Amount << endl; 
+    cout << Game2Quantity << "\t\t" << GameTwoTitle << "\t\t" << Game2Price << "\t\t\t" << Game2Amount << endl;
+    cout << Game3Quantity << "\t\t" << GameThreeTitle << "\t" << Game3Price << "\t\t\t" << Game3Amount << endl;
 
-    std::cout << std::endl;
+    cout << endl;
 
-    std::cout << "\t\t\t\t\tSubtotal\t\t" << subTotal << std::endl;
-    std::cout << "\t\t\t\t\tSales Tax " << SALES_TAX * 100 << "%\t\t" << taxTotal << std::endl;
-    std::cout << "\t\t\t\t\tTotal\t\t\t" << grandTotal << std::endl << std::endl;
-    std::cout << "Thank you and come again!" << std::endl << std::endl;
+    cout << "\t\t\t\t\tSubtotal\t\t" << subTotal << endl;
+    cout << "\t\t\t\t\tSales Tax " << SALES_TAX * 100 << "%\t\t" << taxTotal << endl;
+    cout << "\t\t\t\t\tTotal\t\t\t" << grandTotal << endl << endl;
+    cout << "Thank you and come again!" << endl << endl;
 
     return 0;
 }
